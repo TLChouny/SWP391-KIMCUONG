c6da10c5200033644ba639d9b222659e52171450
DatVX02 <vxdat13@gmail.com>
2024-05-28 18:03:37 +0700

    update

--- FE/confusion/src/App.js
+++ FE/confusion/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./Components/Login/Login";
-
+import Register from "./Components/Register/Register"
 function App() {
   return (
     <Router>
       <>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path='/Register' element={<Register/>}/>
         </Routes>
       </>
     </Router>
