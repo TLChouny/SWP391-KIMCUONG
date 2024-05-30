03cb0e49536d7e48d51f757e7d40ad0653b85f8f
DatVX02 <vxdat13@gmail.com>
2024-05-30 15:35:31 +0700

    prelogin

--- FE/confusion/src/Components/Register/Register.js
+++ FE/confusion/src/Components/Register/Register.js
@@ -1,65 +1,209 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "../Login/Login.css";
-import { Button, Checkbox, Form, Input, message } from 'antd';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+    AutoComplete,
+    Button,
+    Cascader,
+    Checkbox,
+    Col,
+    Form,
+    Input,
+    InputNumber,
+    Row,
+    Select,
+} from 'antd';
+import "../Register/Register.css"
+const { Option } = Select;
 
-const URL = "https://6655a46c3c1d3b60293a7e4a.mockapi.io/login";
-
-export default function Login() {
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-
-    const onFinish = async (values) => {
-        setLoading(true);
-        try {
-            const response = await fetch(URL);
-            const data = await response.json();
-            const { username, password } = values;
-            const user = data.find(item => item.username === username && item.password === password);
-            if (user) {
-                console.log('Login successful:', user);
-                // Navigate to the desired page upon successful login
-                navigate("/");
-            } else {
-                console.log('Incorrect username or password');
-                message.error('Incorrect username or password');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            message.error('An error occurred. Please try again later.');
-        }
-        setLoading(false);
+const formItemLayout = {
+    labelCol: {
+        xs: {
+            span: 24,
+        },
+        sm: {
+            span: 8,
+        },
+    },
+    wrapperCol: {
+        xs: {
+            span: 24,
+        },
+        sm: {
+            span: 16,
+        },
+    },
+};
+const tailFormItemLayout = {
+    wrapperCol: {
+        xs: {
+            span: 24,
+            offset: 0,
+        },
+        sm: {
+            span: 16,
+            offset: 8,
+        },
+    },
+};
+const Register = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
     };
+    const prefixSelector = (
+        <Form.Item name="prefix" noStyle>
+            <Select
+                style={{
+                    width: 70,
+                }}
+            >
+                <Option value="84">+84</Option>
+                {/* <Option value="87">+87</Option> */}
+            </Select>
+        </Form.Item>
+    );
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
-
-    const validateUsername = (_, value) => {
-        if (value && !value.endsWith("@gmail.com")) {
-            return Promise.reject(new Error('Username must end with "@gmail.com"'));
-        }
-        return Promise.resolve();
-    };
 
     return (
         <div className="signin-container">
             <div className="background-image-container">
                 <img src="../assets/background.png" alt="Background" className="background-image" />
             </div>
-
-            <div className="form-login">
+            <div className="form-register">
                 <div className="background-image">
                     <img src="../assets/logo.jpg" alt="Logo" className="logo" />
                     <h4>KLARE</h4>
                 </div>
 
-                <div className="background-login">
+                <div className="background-login-register">
                     <h1>SIGN UP</h1>
-                   
+                    <Form
+                        {...formItemLayout}
+                        form={form}
+                        name="register"
+                        onFinish={onFinish}
+                        initialValues={{
+                            residence: [],
+                            prefix: '+84',
+                        }}
+                        style={{
+                            maxWidth: 600,
+                        }}
+                        scrollToFirstError
+                        className="formlogin"
+                    >
+                        <Form.Item
+                            name="Firstname"
+                            label="Firstname"
+                            rules={[
+                                {
+                                    type: 'text',
+                                },
+                                {
+                                    required: true,
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="Lastname"
+                            label="Lastname"
+                            rules={[
+                                {
+                                    type: 'text',
+                                },
+                                {
+                                    required: true,
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="password"
+                            label="Password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                            hasFeedback
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="confirm"
+                            label="Confirm Password"
+                            dependencies={['password']}
+                            hasFeedback
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please confirm your password!',
+                                },
+                                ({ getFieldValue }) => ({
+                                    validator(_, value) {
+                                        if (!value || getFieldValue('password') === value) {
+                                            return Promise.resolve();
+                                        }
+                                        return Promise.reject(new Error('The new password that you entered do not match!'));
+                                    },
+                                }),
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="email"
+                            label="E-mail"
+                            rules={[
+                                {
+                                    type: 'email',
+                                    message: 'The input is not valid E-mail!',
+                                },
+                                {
+                                    required: true,
+                                    message: 'Please input your E-mail!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+
+
+                        <Form.Item
+                            name="phone"
+                            label="Phone Number"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your phone number!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                addonBefore={prefixSelector}
+                                style={{
+                                    width: '100%',
+                                }}
+                            />
+                        </Form.Item>
+
+                        <Form.Item {...tailFormItemLayout}>
+                            <Button type="primary" htmlType="submit" className="submit">
+                                Register
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </div>
             </div>
         </div>
     );
-}
+};
+export default Register;
