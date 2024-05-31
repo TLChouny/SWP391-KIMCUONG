bbecbe5c6cfc8a4d1dd1d5007df38c5d78cdff32
Long Chau <chauphan2107@gmail.com>
2024-05-31 15:58:17 +0700

    up home

--- FE/confusion/src/Components/Home/Home.js
+++ FE/confusion/src/Components/Home/Home.js
@@ -1,22 +1,61 @@
 import React from "react";
 import "./Home.css";
 import { Input } from 'antd';
-import { SearchOutlined } from '@ant-design/icons'
+import { SearchOutlined } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 
-const App = () => <Input placeholder="Basic usage" />;
-export default function Home() {
+function Header() {
     return (
         <div className="Header">
             <div className="Header_Search">
-                <Input />
+                <Input placeholder="Search..." />
             </div>
-            <div className="Search"><SearchOutlined className="Searchicon" /></div>
+            <div>
+                <SearchOutlined className="Searchicon" />
+            </div>
             <div className="Header_Logo">
-                <img src="../assets/logo.jpg" alt="Logo" className="logo1"></img>
+                <img src="../assets/logo.jpg" alt="Logo" className="logo1" />
             </div>
+            <div className="Header_Name">Klare</div>
             <div className="Header_Account_Cart">
-
+                <UserOutlined className="Usericon" />
+                <ShoppingCartOutlined className="Carticon" />
+            </div>
+            <div className="Header_Ads">
+                <img src="../assets/logo.jpg" alt="Logo" className="logo1" />
+                <img src="../assets/logo.jpg" alt="Logo" className="logo1" />
+                <img src="../assets/logo.jpg" alt="Logo" className="logo1" />
             </div>
         </div>
-    )
+    );
+}
+
+function Body() {
+    return (
+        <div className="Body">
+            {/* Thêm nội dung cho phần Body tại đây */}
+            <img src="../assets/logo.jpg" alt="Logo" className="logo2" />
+            <img src="../assets/logo.jpg" alt="Logo" className="logo2" />
+            <img src="../assets/logo.jpg" alt="Logo" className="logo2" />
+        </div>
+    );
+}
+
+function Footer() {
+    return (
+        <div className="Footer">
+            {/* Thêm nội dung cho phần Footer tại đây */}
+        </div>
+    );
+}
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    );
 }
