da32d97dd66afc44b95dd9a10cae1ccf548b3a2a
DatVX02 <vxdat13@gmail.com>
2024-06-03 17:12:56 +0700

    fix

--- FE/confusion/src/App.js
+++ FE/confusion/src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Prelogin from "./Components/Prelogin/Prelogin";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register"
-import Home from "./Components/Home/Home"
+// import Home from "./Components/Home/Home"
 function App() {
   return (
     <Router>
@@ -13,7 +13,7 @@ function App() {
           <Route path='/prelogin' element={<Prelogin/>}/>
           <Route path="/login" element={<Login />} />
           <Route path='/Register' element={<Register/>}/>
-          <Route path='/Home' element={<Home/>}/>
+          {/* <Route path='/Home' element={<Home/>}/> */}
 
         </Routes>
       </>
