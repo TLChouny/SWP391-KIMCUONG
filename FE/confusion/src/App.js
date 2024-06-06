cd8830a8e004eb49e77bd6660d44c59be2855e6d
Long Chau <chauphan2107@gmail.com>
2024-06-07 01:30:27 +0700

    em up lên rồi anh chỉnh giúp em cái cuộn i ròi em làm típ anh ơi

--- FE/confusion/src/App.js
+++ FE/confusion/src/App.js
@@ -1,21 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
-// import './App.css';
 import Prelogin from "./Components/Prelogin/Prelogin";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register"
-import Homepage from './Components/Homepage/Homepage';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <Router>
       <>
         <Routes>
-          <Route path='/' element={<Homepage/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/prelogin' element={<Prelogin/>}/>
           <Route path="/login" element={<Login />} />
           <Route path='/Register' element={<Register/>}/>
-          
+          <Route path='/Header' element={<Header/>}/>
         </Routes>
       </>
     </Router>
--- FE/confusion/src/Components/Home/Home.js
+++ FE/confusion/src/Components/Home/Home.js
@@ -1,52 +1,73 @@
-import React, { useState } from "react";
-import "../Home/Home.css";
-import { Button, Input } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import { UserOutlined } from '@ant-design/icons';
-import { ShoppingCartOutlined } from '@ant-design/icons';
+import React from "react";
 import { Link } from "react-router-dom";
-function Header({ onSearch }) {
-    const [searchValue, setSearchValue] = useState("");
+import "../Home/Home.css";
+import Header from "../Header/Header";
+// import Footer from "../Footer/Footer";
 
-    const handleSearch = () => {
-        onSearch(searchValue);
-    };
-
-    const handleInputChange = (e) => {
-        setSearchValue(e.target.value);
-    };
-
+const Home = () => {
+    
     return (
-        <div className="Header">
-            <div className="Header_Search">
-                <Input
-                    placeholder="Search..."
-                    value={searchValue}
-                    onChange={handleInputChange}
-                />
-                <Button onClick={handleSearch}><SearchOutlined /></Button>
+        <div className="body" id="scrollContainer" >
+            <Header />
+            <div className="content">
+                <div className="welcome">
+                    <div className="image-container">
+                        <img src="../assets/bracelets.png" alt="Logo" className="logo1" />
+                    </div>
+                    <div className="image-container">
+                        <img src="../assets/necklaces.png" alt="Logo" className="logo2" />
+                    </div>
+                    <div className="image-container">
+                        <img src="../assets/hand.png" alt="Logo" className="logo3" />
+                    </div>
+                </div>
+                <p className="image-text">Welcome to Klare Jewelers</p>
             </div>
-            <div className="Header_Logo">
-                <Link to="/"><img src="../assets/logo.jpg" alt="Logo" className="logo1" /></Link>
-                <div className="Header_Name">Klare</div>
+
+            <div className="sale">
+                {/* <Link to="/sale">Click here for our latest sale!</Link> */}
             </div>
-            <div className="Header_Account_Cart">
-                <Link to="/Prelogin"><UserOutlined className="Usericon" /></Link>
-                <ShoppingCartOutlined className="Carticon" />
+
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
             </div>
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>     <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>     <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>     <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>     <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>     <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>    <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>
+            <div className="news-info">
+                <Link to="/news">Check out our latest news and information.</Link>
+            </div>
+            {/* <Footer />     */}
         </div>
+        
     );
-}
+};
 
-export default function Home() {
-    const handleSearch = (query) => {
-        console.log("Search query:", query);
-        // Thêm logic xử lý tìm kiếm tại đây, ví dụ gọi API hoặc lọc dữ liệu
-    };
-
-    return (
-        <>
-            <Header onSearch={handleSearch} />
-        </>
-    );
-}
+export default Home;
