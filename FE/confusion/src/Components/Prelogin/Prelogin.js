5c1c8173abd47306546c574f88f61db9fb2119b5
DatVX02 <vxdat13@gmail.com>
2024-06-16 12:45:28 +0700

    update FE

--- FE/confusion/src/Components/Prelogin/Prelogin.js
+++ FE/confusion/src/Components/Prelogin/Prelogin.js
@@ -14,14 +14,12 @@ export default function Prelogin() {
 
             <div className="form-prelogin">
                 <div className="background-image">
-                    <Link to = "/"><img src="../assets/logo.jpg" alt="Logo" className="logo" /></Link>
+                    <img src="../assets/logo.jpg" alt="Logo" className="logo" />
                     <h4>KLARE</h4>
                 </div>
 
                 <div className="background-login-pre">
                     <h4>PLEASE YOUR CHOICE LOGIN OR REGISTER</h4>
-
-
                     <Flex gap="small" wrap>
                         <Link to="/login"><Button type="primary" className="buttonchange">Login</Button></Link>
                         <Link to="/Register"><Button type="primary" className="buttonchangesign">Sign up </Button></Link>
