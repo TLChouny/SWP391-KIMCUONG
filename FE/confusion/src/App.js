9c8cc52c5b8a653170d616d423b53681525b8db0
DatVX02 <vxdat13@gmail.com>
2024-06-16 13:15:45 +0700

    update

--- FE/confusion/src/App.js
+++ FE/confusion/src/App.js
@@ -3,15 +3,17 @@ import Prelogin from "./Components/Prelogin/Prelogin";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register"
 import Homepage from "./Components/Homepage/Homepage"
+import ManageAccount from "./Components/Admin/ManageAccount"
 function App() {
   return (
     <Router>
       <>
         <Routes>
           <Route path='/' element={<Homepage/>}/>
-          <Route path='/prelogin' element={<Prelogin/>}/>
-          <Route path="/login" element={<Login />} />
+          <Route path='/Prelogin' element={<Prelogin/>}/>
+          <Route path="/Login" element={<Login />} />
           <Route path='/Register' element={<Register/>}/>
+          <Route path='/Admin' element={<ManageAccount/>}/>
         </Routes>
       </>
     </Router>
