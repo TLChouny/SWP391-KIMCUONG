18293213328d28d5a43bfb2adc45cb687e3e3a15
DatVX02 <vxdat13@gmail.com>
2024-06-17 16:24:21 +0700

    update

--- FE/confusion/src/Components/Homepage/Homepage.js
+++ FE/confusion/src/Components/Homepage/Homepage.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Header from "../Header/Header";
 import Menuheader from "../Menu/Menuheader"
-
+import Slider from "../Slider/Slider"
+import "../Homepage/Homepage.css"
 export default function Homepage() {
     return (
-        <div>
+        <div style={{ height: '100vh', overflowY: 'auto' }}>
             <Header/>
             <Menuheader/>
+            <Slider/>
             
         </div>
     )
