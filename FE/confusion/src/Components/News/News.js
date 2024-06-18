96b5d9a42e75952001ba02f7fda38b37b48269d0
DatVX02 <vxdat13@gmail.com>
2024-06-18 18:35:26 +0700

    update data news

--- FE/confusion/src/Components/News/News.js
+++ FE/confusion/src/Components/News/News.js
@@ -1,30 +1,21 @@
 import React from "react";
-import "../News/News.css"
+import "../News/News.css";
+import news from "../../data/news.json";
+
 export default function News() {
     return (
         <div className="News">
             <h1>News</h1>
-            <h3>chọn Jemmia diamond bạn được gì</h3>
-            <div>
-                <div>
-                    <img src="../assets/diamond.svg" />
-                    <p>CHẤT LƯỢNG ĐẠT CHUẨN</p>
-                    <p>Cam kết đún trọng lượng và hàm lượng. Kim cương và đá quý đều được nhập khẩu chính gạch 100%, được kiểm định quốc tế và có giá trị toàn cầu.</p>
-                </div>
-
-                <div>
-                    <img src="../assets/493808.png" />
-                    <p>PHỤC VỤ TẬN TÂM</p>
-                    <p>Đội ngũ chuyên gia chăm sóc khách hàng dày dặn kinh nghiệm luôn có mặt để giúp đỡ và tư vấn chọn món trang sức phù hợp nhất cho quý khách.</p>
-                </div>
-
-                <div>
-                    <img src="../assets/f44a5f5a0a014383f7e102f349bd6c9b.jpg" />
-                    <p>BẢO HÀNH TRỌN ĐỜI</p>
-                    <p>Miễn phí làm mới sản phẩm trọn đời. Chế độ 1 đổi 1 và hỗ trợ các chính sách thu mua thu đổi có lợi nhất cho quý khách.</p>
-                </div>
+            <h3>WHAT DO YOU GET BY CHOOSING THE DIAMOND SHOP?</h3>
+            <div className="news-container">
+                {news.map((newsItem, index) => (
+                    <div className="news-item" key={index}>
+                        <img src={newsItem.image} alt="News" />
+                        <h4>{newsItem.name}</h4>
+                        <p>{newsItem.title}</p>
+                    </div>
+                ))}
             </div>
         </div>
-    )
-
+    );
 }
