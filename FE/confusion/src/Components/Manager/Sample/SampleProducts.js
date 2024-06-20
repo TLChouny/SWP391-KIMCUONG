0717c48fa354e7325f79907b6ccd1d107d8dcfb1
Long Chau <chauphan2107@gmail.com>
2024-06-21 00:57:33 +0700

    up manage product

---
+++ FE/confusion/src/Components/Manager/Sample/SampleProducts.js
@@ -0,0 +1,224 @@
+const SampleProducts = [
+    {
+        id: '1',
+        name: 'Product 1',
+        image: 'image1.jpg',
+        mainIngredient: 'Ingredient A',
+        secondaryIngredient: 'Ingredient B',
+        cover: 'Cover 1',
+        type: 'Type 1',
+        price: 100,
+        quantity: 10
+    },
+    {
+        id: '2',
+        name: 'Product 2',
+        image: 'image2.jpg',
+        mainIngredient: 'Ingredient C',
+        secondaryIngredient: 'Ingredient D',
+        cover: 'Cover 2',
+        type: 'Type 2',
+        price: 200,
+        quantity: 20
+    },
+    {
+        id: '3',
+        name: 'Product 3',
+        image: 'image3.jpg',
+        mainIngredient: 'Ingredient E',
+        secondaryIngredient: 'Ingredient F',
+        cover: 'Cover 3',
+        type: 'Type 3',
+        price: 300,
+        quantity: 30
+    },
+    {
+        id: '4',
+        name: 'Product 4',
+        image: 'image4.jpg',
+        mainIngredient: 'Ingredient G',
+        secondaryIngredient: 'Ingredient H',
+        cover: 'Cover 4',
+        type: 'Type 4',
+        price: 400,
+        quantity: 40
+    },
+    {
+        id: '5',
+        name: 'Product 5',
+        image: 'image5.jpg',
+        mainIngredient: 'Ingredient I',
+        secondaryIngredient: 'Ingredient J',
+        cover: 'Cover 5',
+        type: 'Type 5',
+        price: 500,
+        quantity: 50
+    },
+    {
+        id: '6',
+        name: 'Product 6',
+        image: 'image6.jpg',
+        mainIngredient: 'Ingredient K',
+        secondaryIngredient: 'Ingredient L',
+        cover: 'Cover 6',
+        type: 'Type 6',
+        price: 600,
+        quantity: 60
+    },
+    {
+        id: '7',
+        name: 'Product 7',
+        image: 'image7.jpg',
+        mainIngredient: 'Ingredient M',
+        secondaryIngredient: 'Ingredient N',
+        cover: 'Cover 7',
+        type: 'Type 7',
+        price: 700,
+        quantity: 70
+    },
+    {
+        id: '8',
+        name: 'Product 8',
+        image: 'image8.jpg',
+        mainIngredient: 'Ingredient O',
+        secondaryIngredient: 'Ingredient P',
+        cover: 'Cover 8',
+        type: 'Type 8',
+        price: 800,
+        quantity: 80
+    },
+    {
+        id: '9',
+        name: 'Product 9',
+        image: 'image9.jpg',
+        mainIngredient: 'Ingredient Q',
+        secondaryIngredient: 'Ingredient R',
+        cover: 'Cover 9',
+        type: 'Type 9',
+        price: 900,
+        quantity: 90
+    },
+    {
+        id: '10',
+        name: 'Product 10',
+        image: 'image10.jpg',
+        mainIngredient: 'Ingredient S',
+        secondaryIngredient: 'Ingredient T',
+        cover: 'Cover 10',
+        type: 'Type 10',
+        price: 1000,
+        quantity: 100
+    },
+    {
+        id: '11',
+        name: 'Product 11',
+        image: 'image11.jpg',
+        mainIngredient: 'Ingredient U',
+        secondaryIngredient: 'Ingredient V',
+        cover: 'Cover 11',
+        type: 'Type 11',
+        price: 1100,
+        quantity: 110
+    },
+    {
+        id: '12',
+        name: 'Product 12',
+        image: 'image12.jpg',
+        mainIngredient: 'Ingredient W',
+        secondaryIngredient: 'Ingredient X',
+        cover: 'Cover 12',
+        type: 'Type 12',
+        price: 1200,
+        quantity: 120
+    },
+    {
+        id: '13',
+        name: 'Product 13',
+        image: 'image13.jpg',
+        mainIngredient: 'Ingredient Y',
+        secondaryIngredient: 'Ingredient Z',
+        cover: 'Cover 13',
+        type: 'Type 13',
+        price: 1300,
+        quantity: 130
+    },
+    {
+        id: '14',
+        name: 'Product 14',
+        image: 'image14.jpg',
+        mainIngredient: 'Ingredient AA',
+        secondaryIngredient: 'Ingredient BB',
+        cover: 'Cover 14',
+        type: 'Type 14',
+        price: 1400,
+        quantity: 140
+    },
+    {
+        id: '15',
+        name: 'Product 15',
+        image: 'image15.jpg',
+        mainIngredient: 'Ingredient CC',
+        secondaryIngredient: 'Ingredient DD',
+        cover: 'Cover 15',
+        type: 'Type 15',
+        price: 1500,
+        quantity: 150
+    },
+    {
+        id: '16',
+        name: 'Product 16',
+        image: 'image16.jpg',
+        mainIngredient: 'Ingredient EE',
+        secondaryIngredient: 'Ingredient FF',
+        cover: 'Cover 16',
+        type: 'Type 16',
+        price: 1600,
+        quantity: 160
+    },
+    {
+        id: '17',
+        name: 'Product 17',
+        image: 'image17.jpg',
+        mainIngredient: 'Ingredient GG',
+        secondaryIngredient: 'Ingredient HH',
+        cover: 'Cover 17',
+        type: 'Type 17',
+        price: 1700,
+        quantity: 170
+    },
+    {
+        id: '18',
+        name: 'Product 18',
+        image: 'image18.jpg',
+        mainIngredient: 'Ingredient II',
+        secondaryIngredient: 'Ingredient JJ',
+        cover: 'Cover 18',
+        type: 'Type 18',
+        price: 1800,
+        quantity: 180
+    },
+    {
+        id: '19',
+        name: 'Product 19',
+        image: 'image19.jpg',
+        mainIngredient: 'Ingredient KK',
+        secondaryIngredient: 'Ingredient LL',
+        cover: 'Cover 19',
+        type: 'Type 19',
+        price: 1900,
+        quantity: 190
+    },
+    {
+        id: '20',
+        name: 'Product 20',
+        image: 'image20.jpg',
+        mainIngredient: 'Ingredient MM',
+        secondaryIngredient: 'Ingredient NN',
+        cover: 'Cover 20',
+        type: 'Type 20',
+        price: 2000,
+        quantity: 200
+    }
+];
+
+export default SampleProducts;
